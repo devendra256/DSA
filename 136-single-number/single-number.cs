c308c7a0a9a75104716bb6272275c45b1d4904d2
Devendra Sharma <72224432+devendra256@users.noreply.github.com>
2026-07-07 01:08:41 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (65.68%) - LeetSync

--- 136-single-number/single-number.cs
+++ 136-single-number/single-number.cs
@@ -1,21 +1,11 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
 
-        Dictionary<int, int> dict = new Dictionary<int, int>();
-
+        int result = 0;
+        
         foreach(int num in nums) {
-            if(dict.ContainsKey(num)) {
-                dict[num]++;
-            } else {
-                dict[num] = 1;
-            }
+            result ^= num;
         }
-
-        foreach(var keyVal in dict) {
-            if(keyVal.Value == 1) {
-                return keyVal.Key;
-            }
-        }
-        return -1;
+        return result;
     }
 }
